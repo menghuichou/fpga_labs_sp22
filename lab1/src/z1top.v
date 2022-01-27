d5fe7bf8db34ab20a2b06d7ab894a3964ef2c2ea
menghuichou <cmh890410@gmail.com>
2022-01-26 17:25:01 -0800

    finish lab1

--- lab1/src/z1top.v
+++ lab1/src/z1top.v
@@ -6,6 +6,12 @@ module z1top(
   input [1:0] SWITCHES,
   output [5:0] LEDS
 );
-  and(LEDS[0], BUTTONS[0], SWITCHES[0]);
-  assign LEDS[5:1] = 0;
+  //and(LEDS[0], BUTTONS[0], SWITCHES[0]);
+  wire or_02, and_13;
+  or(or_02, BUTTONS[0], BUTTONS[2]);
+  and(and_13, BUTTONS[1], BUTTONS[3]);
+  or(LEDS[1], or_02, and_13);
+  assign LEDS[0] = 0;
+  assign LEDS[5:2] = 0;
+  //assign LEDS[5:1] = 0;
 endmodule
